88c21f69dcb29930a470a06a9d613611f4241faf
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-09 15:44:40 -0600

    seeds database 2users 4messages 6omments

--- db/migrate/20161209200634_create_comments.rb
+++ db/migrate/20161209200634_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :message_id
       t.text :text
       t.references :message
       t.references :user
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20161209200634) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "message_id"
     t.text     "text"
+    t.integer  "message_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["message_id"], name: "index_comments_on_message_id", using: :btree
@@ -27,8 +27,11 @@ ActiveRecord::Schema.define(version: 20161209200634) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
+    t.text     "website"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
