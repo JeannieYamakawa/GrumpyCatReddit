0e31a721bbeb6aeebbdad8c2b908f6955d6ce98a
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-12 15:08:29 -0600

    user patch functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,35 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+
+
     def show
         @user = User.find(params[:id])
         @messages = @user.messages
     end
+
+
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            redirect_to(:action => 'show', :id => @user.id)
+        else
+            render 'edit'
+        end
+    end
+
+
+    private
+    def user_params
+        params.require(:user).permit(:username)
+    end
+
+
+
+
+
 end
