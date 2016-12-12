fd2d76068162d08bef8fdc2c1e192f16d8ce2c24
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-12 12:37:15 -0600

    reconfigures routes to resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
     # get 'welcome' => 'pages#home'
-    get "/messages" => "messages#index"
-    get "/messages/new" => "messages#new"
-    post 'messages' => 'messages#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users do
+        resources :messages do
+            resources :comments
+        end
+    end
 end
- 
