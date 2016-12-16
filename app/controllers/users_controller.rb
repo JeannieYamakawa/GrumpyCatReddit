65780671e179b3d2bf13b7184d4b0ec8675ae88f
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-15 23:32:51 -0600

    more styling and home button added to pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
 
     def show
-        @user = User.find(params[:id])
+        @user = User.find(params[:id]) 
         @messages = @user.messages
     end
 
